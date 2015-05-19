aff38aafddd2b25b8677fd44c1bceaa609c9a4cd
Pascal <krohpasc@hu-berlin.de>
2015-05-19 17:42:44 +0200

    Neu

--- 02_introduction/guess_a_number_pk.py
+++ 02_introduction/guess_a_number_pk.py
@@ -6,17 +6,22 @@ def guess():
     x = int(raw_input('Whats your guess? '))
     count_guess = 1
     while x != n:
-    	if x < n:
-    		print 'Keep guessing, the right number is bigger than yours!'
-    		x = int(raw_input('Whats your new guess? '))
-    		count_guess = count_guess + 1
-    	else:
-    		print 'Keep guessing, the right number is smaler than yours!'
-    		x = int(raw_input('Whats your new guess? '))
-    		count_guess = count_guess + 1
+        if x < n:
+            print 'Keep guessing, the right number is bigger than yours!'
+            x = int(raw_input('Whats your new guess? '))
+            count_guess = count_guess + 1
+        else:
+            print 'Keep guessing, the right number is smaler than yours!'
+            x = int(raw_input('Whats your new guess? '))
+            count_guess = count_guess + 1
     if x == n:
-    	print 'Awesome! You are right!'
-    	print 'Your needed counts:'
-    	print count_guess
+        print 'Awesome! You are right!'
+        print 'Your needed counts:'
+        print count_guess
+        y = raw_input('Do you want to play again? ')
+        if y == 'yes':
+            guess()
+        else:
+            print 'Goodbye!'
 
 guess()
