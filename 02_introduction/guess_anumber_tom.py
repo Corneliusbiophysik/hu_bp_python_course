2a8af1e487e67b5245b77614a28876dc4e0df982
PoolUser <biostudent204@fredda.biologie.hu-berlin.de>
2015-05-19 15:58:10 +0200

    changed file

---
+++ 02_introduction/guess_anumber_tom.py
@@ -0,0 +1,26 @@
+from random import randint
+
+def main():
+	zahl=randint(1,100)
+
+	while True:
+		try: 
+			geraten=int(raw_input('Rate eine Zahl zwischen 1 und 100:'))
+			if geraten < 1 or geraten > 99:
+				continue
+		except: 
+			print 'Gib bitte eine ganze Zahl zwischen 1 und 100 ein!'
+			continue
+		if geraten < zahl:
+			print u'Du hast zu klein geschaetzt.'
+		elif geraten > zahl:
+			print u'Das ist leider zu hoch!'
+		elif geraten == zahl:
+			print "Richtig!"
+			break
+	print geraten
+
+
+if __name__ == '__main__':
+	main()
+
