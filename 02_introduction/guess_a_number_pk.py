e4e9866bce10f17a008c21f6398b938cb30aa84a
Pascal <krohpasc@hu-berlin.de>
2015-05-19 17:31:25 +0200

    Spiel erstellt

---
+++ 02_introduction/guess_a_number_pk.py
@@ -0,0 +1,22 @@
+from random import randint
+
+def guess():
+    n=randint(0, 100)
+    print 'Guess a number between 0 and 100!'
+    x = int(raw_input('Whats your guess? '))
+    count_guess = 1
+    while x != n:
+    	if x < n:
+    		print 'Keep guessing, the right number is bigger than yours!'
+    		x = int(raw_input('Whats your new guess? '))
+    		count_guess = count_guess + 1
+    	else:
+    		print 'Keep guessing, the right number is smaler than yours!'
+    		x = int(raw_input('Whats your new guess? '))
+    		count_guess = count_guess + 1
+    if x == n:
+    	print 'Awesome! You are right!'
+    	print 'Your needed counts:'
+    	print count_guess
+
+guess()
