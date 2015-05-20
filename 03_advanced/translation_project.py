bfe44e77bd76934555d47095cfe2dc03f115af6e
Pascal <krohpasc@hu-berlin.de>
2015-05-20 14:35:07 +0200

    BioMolecule.py hinzugefügt

--- 03_advanced/translation_project.py
+++ 03_advanced/translation_project.py
@@ -11,9 +11,39 @@ class BioMolecule(object):
     @type mass: float
     """
     def __init__(self, id, name, mass=None):
-        self._id = id
+        self.__id = id
         self.name = name
         self.mass = mass
+
+    @property
+    def mass(self):
+        return self.__mass
+
+    @mass.setter
+    def mass(self, value):
+        if not isinstance(value, float):
+            raise TypeError('Mass must be integer.')
+        self.__mass = value
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, word):
+        if not isinstance(word, str):
+            raise TypeError('Mass must be string.')
+        self.__name = word
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, number):
+        if not isinstance(number, int):
+            raise TypeError('ID must be integer.')
+        self.__id = number
 
     # 1. Write setter and getter methods for all attributes.
     #    Use @property decorators as dicussed in the lecture
@@ -29,9 +59,20 @@ class Polymer(BioMolecule):
     @type sequence: str
     @type mass: float
     """
-    def __init__(self, id, name, sequence, mass=None):
+    def __init__(self, id, sequence, name, mass=None):
+        super(Polymer, self).__init__(id, name, mass)
         # 3. Initialize the parent class correctly
-        self._sequence = sequence
+        self.__sequence = sequence
+
+    @property
+    def sequence(self):
+        return self.__sequence
+
+    @sequence.setter
+    def sequence(self, words):
+        if not isinstance(words, str):
+            raise TypeError('Sequence must be string.')
+        self.__sequence = words
 
     
     # 4. Write getter and setter for sequence, again check for type
